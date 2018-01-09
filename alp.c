97296d7afcc783ac8e8115d3945a2f5e42ecf4d4
heerakrishnasamy <30829823+heerakrishnasamy@users.noreply.github.com>
2018-01-09 21:38:21 +0530

    Update alp.c

--- alp.c
+++ alp.c
@@ -8,7 +8,7 @@ int main()
     if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
         printf("%c is an alphabet.",c);
     else
-        printf("%c is not an alphabet.",c);
+        printf("it is not an alphabet.");
 
     return 0;
 }
