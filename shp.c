90c40acf8f5f239dbd26295bc0de2b2248e2fb4f
heerakrishnasamy <30829823+heerakrishnasamy@users.noreply.github.com>
2018-01-11 06:52:09 +0530

    Update shp.c

--- shp.c
+++ shp.c
@@ -6,10 +6,10 @@ printf("enter a character");
 scanf("%c",&c);
 if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
 {
-printf("vowel");
+printf("Vowel");
 }
 else
 {
-printf("consonant");
+printf("Consonant");
 }
 }
