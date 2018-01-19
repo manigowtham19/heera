829c3d07abafc00b85a7483cf43fa0e86a80de99
heerakrishnasamy <30829823+heerakrishnasamy@users.noreply.github.com>
2018-01-19 08:37:32 +0000

    Update int.c

--- int.c
+++ int.c
@@ -3,11 +3,11 @@ void main()
 {
 int a[10],n,k,i,sum=0;
 scanf("%d%d",&n,&k);
-for(i=0;i<=n;i++)
+for(i=1;i<=n;i++)
 {
   printf("%d\t",a[i]);
 }
-for(i=0;i<=k;i++)
+for(i=1;i<=k;i++)
 {
   sum=sum+a[i];
 }
