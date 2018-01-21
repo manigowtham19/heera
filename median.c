9fad2beb306e11007e8b22ce55a08a6b34dabf41
heerakrishnasamy <30829823+heerakrishnasamy@users.noreply.github.com>
2018-01-21 12:16:39 +0530

    Update median.c

--- median.c
+++ median.c
@@ -8,7 +8,21 @@ int main()
     {
         scanf("%d",&a[i]);
     }
+    for(j=1;j<n;j++)
+    {
+        for(k=j+1;k<n;k++)
+        {
+            if(a[j]>a[k])
+            {
+                temp=a[j];
+                a[j]=a[k];
+                a[k]=temp;
+                
+            }
+        }
+        printf("%d\t",a[j]);
     n=(n+1)/2-1;
     printf("%d",a[n]);
+    }
     return 0;
 }
