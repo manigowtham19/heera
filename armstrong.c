363566bfad408270f9b3dcea3c43b4f6469cf415
heerakrishnasamy <30829823+heerakrishnasamy@users.noreply.github.com>
2018-01-21 10:11:43 +0530

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -9,14 +9,14 @@ while(t!=0)
 rem=t%10;
 s=s+rem*rem*rem;
 t=t/10;
-	}
-	if(s==n)
-	{
-		printf("yes");
-	}
-	else
-	{
-		printf("no");
-	}
-	return 0;
-	}
+}
+if(s==n)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
