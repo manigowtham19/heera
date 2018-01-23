547b31c0054cd6b040d3961998b2c7b2d33b6c51
heerakrishnasamy <30829823+heerakrishnasamy@users.noreply.github.com>
2018-01-23 16:02:03 +0530

    Create maximum.c

---
+++ maximum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a[20],i,n,l;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+l=a[0];
+for(i=0;i<n;i++)
+{
+if(l<a[i])
+{
+l=a[i];
+}
+}
+printf("%d",l);
+return 0;
+}
