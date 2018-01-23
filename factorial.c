acb18bba0756d83c65f0b159057608c8657a3a68
heerakrishnasamy <30829823+heerakrishnasamy@users.noreply.github.com>
2018-01-23 15:49:35 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 void main()
 {
-int i,f=1,num;
+int i,fact=1,num;
 scanf("%d",&num);
 for(i=i=1;i<=num;i++)
 {
-f=f*i;
+fact=fact*i;
 }
-printf("%d",num,f);
+printf("%d",num,fact);
 }
