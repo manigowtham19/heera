5b506942d976eab64177c87938fdbffd353070e4
heerakrishnasamy <30829823+heerakrishnasamy@users.noreply.github.com>
2018-02-01 08:29:48 +0000

    Update countword.c

--- countword.c
+++ countword.c
@@ -1,15 +1,16 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
 void main()
 {
-char s[200];
-int count = 0, i;
-printf("enter the string\n");
-scanf("%[^\n]s", s);
-for (i = 0;s[i] != '\0';i++)
-{
-if (s[i] == ' ')
-count++;    
+  char a[100];
+  int count=0,i;
+  printf("enter the string");
+  scanf("%[^\n]a", &a);
+  for(i=0;a[i]!='\0';i++)
+  {
+    if(a[i]==' ')
+    count++;
+  }
+  printf("%d",count+1);
 }
-printf("number of words in given string are: %d\n", count + 1);
-}
+      
