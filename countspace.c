a0744511011345c7db1a2732fb51c3f25601ce6e
heerakrishnasamy <30829823+heerakrishnasamy@users.noreply.github.com>
2018-02-02 14:36:58 +0530

    Create countspace.c

---
+++ countspace.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+  char a[100];
+  int count=0,i;
+  printf("enter the string");
+  scanf("%[^\n]a", &a);
+  for(i=0;a[i]!='\0';i++)
+  {
+    if(a[i]==' ')
+    {
+    count++;
+    }
+  }
+  printf("%d",count);
+}
